26b8a8911769fe4a14f508b0803a11dd255b7968
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-02-19 23:54:26 -0400

    feat: Initialize Next.js project with TypeScript and update core dependencies.

---
+++ starting-project/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Next.js Course (TypeScript Mode)</h1>
+      <p>This is the starting project.</p>
+    </div>
+  )
+}
