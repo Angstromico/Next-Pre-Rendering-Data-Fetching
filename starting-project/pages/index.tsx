3c527a86eae44f40a0e8478bd378f5e778c479ec
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-02-20 00:15:39 -0400

    feat: Use getStaticProps in index.tsx

--- starting-project/pages/index.tsx
+++ starting-project/pages/index.tsx
@@ -1,9 +1,37 @@
-export default function Home() {
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
+
+// 1. Define the internal data structure
+type Product = {
+  id: number
+  name: string
+}
+
+// 2. Use GetStaticProps to type the function
+export const getStaticProps: GetStaticProps<{
+  products: Product[]
+}> = async () => {
+  const products: Product[] = [{ id: 1, name: 'Product 1' }]
+
+  return {
+    props: {
+      products,
+    },
+  }
+}
+
+// 3. Use InferGetStaticPropsType to tell the component exactly what it's receiving
+export default function Home({
+  products = [],
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map(
+        (
+          product, //Cannot read properties of undefined (reading 'map')
+        ) => (
+          <li key={product.id}>{product.name}</li>
+        ),
+      )}
     </ul>
   )
 }
