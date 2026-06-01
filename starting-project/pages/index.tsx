1c0908fe5c7337dbce93f3944f9a8fc158482c4a
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-01 11:56:25 -0400

    refactor: define types folder

--- starting-project/pages/index.tsx
+++ starting-project/pages/index.tsx
@@ -1,20 +1,9 @@
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import path from 'path'
 import fs from 'fs/promises'
-import { title } from 'process'
+import type { Data, Product } from './interfaces'
 
-// 1. Define the internal data structure
-type Product = {
-  id: string
-  title: string
-  description: string
-}
-
-interface Data {
-  products: Product[]
-}
-
-// 2. Use GetStaticProps to type the function
+// 1. Use GetStaticProps to type the function
 export const getStaticProps: GetStaticProps<{
   products: Product[]
 }> = async () => {
@@ -31,7 +20,7 @@ export const getStaticProps: GetStaticProps<{
   }
 }
 
-// 3. Use InferGetStaticPropsType to tell the component exactly what it's receiving
+// 2. Use InferGetStaticPropsType to tell the component exactly what it's receiving
 export default function Home({
   products = [],
 }: InferGetStaticPropsType<typeof getStaticProps>) {
